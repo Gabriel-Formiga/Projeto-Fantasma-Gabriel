a084260dfd87b5209854eb62e0442fafd143f630
Gabriel-Formiga <bulhoesgab@gmail.com>
2025-10-19 16:10:31 -0300

    Entrega anlise 2

--- rdocs/entrega_2.R
+++ rdocs/entrega_2.R
@@ -22,3 +22,31 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
+infos_clientes <- read_excel("~/relatorio_old_town_road (1).xlsx", 
+                             sheet = "infos_clientes")
+
+df <- infos_clientes
+
+df_cliente <- df %>%
+  mutate(altura_cm = Height_dm*10)
+
+df_cliente_final <- df_cliente %>%
+  mutate(peso_kg = Weight_lbs*0.453592)
+
+cor.test(df_cliente_final$altura_cm,df_cliente_final$peso_kg)
+
+ggplot(df_cliente_final) +
+  aes(x = altura_cm, y = peso_kg) +
+  geom_point(colour = "#A11D21", size = 3,
+             alpha = 0.5) +
+  labs(
+    x = "Altura (Centimetros)",
+    y = "Peso (quilogramas)"
+  ) +
+  theme_estat()
+ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
